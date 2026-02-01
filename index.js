0dc8a865ff73b108ad2be5fe228e1ae64da70cbd
alfaqartofficial <alfaqartofficial@gmail.com>
2026-02-01 11:53:42 +0530

    index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.json({ status: "Reviews backend running" });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
